1c0dc99576e43bf4d25f9b0a672a29ce71e3141e
ruffoh <ruffo3636@gmail.com>
2024-12-10 08:53:04 +0100

    Input

--- 2k24/Socket/1.2/client.java
+++ 2k24/Socket/1.2/client.java
@@ -3,13 +3,17 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) {
         try (Socket socket = new Socket("127.0.0.1", 9999)) {
             System.out.println("Connessione al server stabilita");
-
-            String parola = "negro";
+            Scanner in = new Scanner(System.in);
+            System.out.println("Inserisci la parola da inviare che verrà invertita:  ");
+             String parola = in.nextLine();
+             System.out.println("Hai inserito la parola: "+ parola);
+            
 
             // Invio della parola al server
             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
